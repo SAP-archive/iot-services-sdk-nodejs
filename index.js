7db67cd312ef26e56d133ba807d39da89162e436
Punit Ghodasara <punit.ghodasara@sap.com>
2019-04-16 16:22:21 +0100

    Added Index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+const IoTDeviceService = require('./lib/IoTAPIService');
+const IoTMeasureService = require('./lib/IoTMeasureService');
+
+module.exports.IoTDeviceService = IoTDeviceService;
+module.exports.IoTMeasureService = IoTMeasureService;
