3bec03a485458660990c02ccdfbb37492873b4eb
Punit Ghodasara <punit.ghodasara@sap.com>
2019-05-02 09:51:23 +0100

    Minimized packages needed in package.json

--- examples/get/sensorTypePrinting.js
+++ examples/get/sensorTypePrinting.js
@@ -3,8 +3,12 @@ const IoTAPIService = require('../../lib/IoTAPIService');
 
 const iotService = new IoTAPIService();
 const iot = new IoT();
+const tenantId = "885993624";
 
-iotService.list(iot.tenant.sensorTypes, {tenantId: 885993624})
+iotService
+    .list(iot.tenant.sensorTypes, {
+        tenantId: tenantId
+    })
     .then((sensorTypes) => {
         sensorTypes.forEach(sensorType => {
             console.log(sensorType)
