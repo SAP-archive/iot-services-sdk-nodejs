c966e0bebb34d2336564bb0878483b66700ac4c9
Punit Ghodasara <punit.ghodasara@sap.com>
2019-05-02 10:10:46 +0100

    Exporting all other services and model from index

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const IoTDeviceService = require('./lib/IoTAPIService');
 const IoTMeasureService = require('./lib/IoTMeasureService');
+const IoTGatewayService = require('./lib/IoTGatewayService');
+const IoTModel = require('./lib/model/IoT');
 
 module.exports.IoTDeviceService = IoTDeviceService;
+module.exports.IoTMeasureService = IoTMeasureService;
+module.exports.IoTGatewayService = IoTGatewayService;
+module.exports.IoTModel = IoTModel;
